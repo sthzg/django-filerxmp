840894af8b699a11fcd128668e0d47ffc57d8e38
sthzg <sthzg@gmx.net>
2014-11-12 12:33:30 +0100

    Views -- Updated source formatting.

--- filerxmp/views.py
+++ filerxmp/views.py
@@ -1,53 +1,36 @@
 # -*- coding: utf-8 -*-
-# ______________________________________________________________________________
-#                                                                         Future
 from __future__ import absolute_import
-# ______________________________________________________________________________
-#                                                                         Django
+import logging
 from django.http.response import Http404
 from django.views.generic.base import View
-# ______________________________________________________________________________
-#                                                                        Contrib
 from filersets.models import Item
 from rest_framework import viewsets
-# ______________________________________________________________________________
-#                                                                        Package
-from filerxmp.serializers import XMPBaseSerializer, XMPImageSerializer
-from filerxmp.models import XMPBase, XMPImage
-
-
-# ______________________________________________________________________________
-#                                                               View: ProcessXMP
 from rest_framework.generics import RetrieveAPIView
+from .serializers import XMPBaseSerializer, XMPImageSerializer
+from .models import XMPBase, XMPImage
+
+logger = logging.getLogger(__name__)
 
 
 class ProcessXMPView(View):
-
     def get(self, request, file_id=None):
         pass
 
 
-# ______________________________________________________________________________
-#                                                              API View: XMPBase
 class XMPBaseViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    API endpoint that allows XMPBase data to be viewed
-    """
+    """API endpoint that allows XMPBase data to be viewed."""
     queryset = XMPBase.objects.all()
     serializer_class = XMPBaseSerializer
 
 
 class XMPImageViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    API endpoint that allows XMPImage data to be viewed
-    """
+    """API endpoint that allows XMPImage data to be viewed."""
     queryset = XMPImage.objects.all()
     serializer_class = XMPImageSerializer
 
 
 class XMPBaseDetail(RetrieveAPIView):
-    """
-    Return the associated XMP item.
+    """Return the associated XMP item.
 
     Currently supported lookups by:
     - pk_item
