c2aa3d1430a94e04504dd5587e0539f91c6172d7
sthzg <sthzg@gmx.net>
2014-11-12 14:01:12 +0100

    Added get_iptc_keywords function.

--- filerxmp/helpers.py
+++ filerxmp/helpers.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-# ______________________________________________________________________________
-#                                                                         Future
 from __future__ import absolute_import
-# ______________________________________________________________________________
-#                                                                         Python
+import iptcinfo
+from PIL import Image as PILImage
 from dateutil import parser
-# ______________________________________________________________________________
-#                                                                        Contrib
 from pytz import timezone
 
 
 def get_xmp_string_from__file(path):
-    """ This function parses given files for xmp metadata.
+    """This function parses given files for xmp metadata.
 
     :param path: file path to parse
     :rtype: False or XML string
@@ -23,7 +19,7 @@ def get_xmp_string_from__file(path):
 
         xmp_start = img_raw.find('<x:xmpmeta')
         xmp_end = img_raw.find('</x:xmpmeta>')
-        xmp_str = img_raw[xmp_start:xmp_end+12]
+        xmp_str = img_raw[xmp_start:xmp_end + 12]
 
     except IOError:
         return False
@@ -34,8 +30,22 @@ def get_xmp_string_from__file(path):
         return xmp_str
 
 
+def get_iptc_keywords(path):
+    """Returns iptc keywords or empty list for file at ``path``."""
+    im = PILImage.open(path)
+    im.verify()
+
+    if im.format == 'JPEG':
+        iptc = iptcinfo.IPTCInfo(path)
+        if len(iptc.keywords) > 0:
+            return iptc.keywords[0].split(',')
+
+    return []
+
+
+
 def get_datetime_or_none(timestring):
-    """ This function takes a string and tries to return a valid datetime.
+    """This function takes a string and tries to return a valid datetime.
 
     :param timestring: string with valid definition of a datetime
     :rtype: datetime object or None
